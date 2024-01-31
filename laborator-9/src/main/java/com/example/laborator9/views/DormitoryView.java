c574772c9c711a86d2b81fa71b4f4b3dee82e807
HAIURA Andreea-Isabela <79141555+isabellla128@users.noreply.github.com>
2024-02-01 01:40:23 +0200

    adding ajax

--- laborator-9/src/main/java/com/example/laborator9/views/DormitoryView.java
+++ laborator-9/src/main/java/com/example/laborator9/views/DormitoryView.java
@@ -7,22 +7,29 @@ import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-//@Getter
-//@ViewScoped
-//public class DormitoryView {
-//    private List<String> dormitories;
-//    private List<String> rooms ;
-//
-//    @Inject
-//    private CampusRetriever campusRetriever;
-//
-//    @PostConstruct
-//    public void init() {
-//        dormitories = campusRetriever.getDormitoriesNames();
-//        rooms = new ArrayList<>();
-//    }
-//}
+@Getter
+@Named("DormitoryView")
+@ViewScoped
+public class DormitoryView implements Serializable {
+    private List<String> dormitories;
+    private List<String> rooms;
+
+    @Inject
+    private CampusRetriever campusRetriever;
+
+    @PostConstruct
+    public void init() {
+        dormitories = campusRetriever.getDormitoriesNames();
+        rooms = campusRetriever.getRooms(dormitories.get(0));
+    }
+
+    public void onDormitoryChange(String dormitory) {
+        // You can implement the logic to retrieve rooms based on the selected dormitory here
+        // For example, if you have a method in campusRetriever to get rooms by dormitory:
+        rooms = campusRetriever.getRooms(dormitory);
+    }
+}
